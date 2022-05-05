ff20ca41ade3c60931401940b01c51456ef685f5
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2022-05-05 20:02:31 +0200

    Upgraded to React 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import tileGame from './reducers/tile-game-reducer';
@@ -19,11 +19,14 @@ const store = createStore(tileGame, composeEnhancers(
 
 store.dispatch(initGame(GameId_3x3, Math.floor(Math.random() * NumImages) + 1, true));
 store.dispatch(fetchHighScoreList);
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
     <Provider store={store}>
         <GameView />
     </Provider>
-    , document.getElementById('root'));
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
